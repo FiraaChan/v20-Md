d88fc8910d3c5d8413da33d47be8b34c8f9c492f
F I R A B O T Z <99767874+FiraaChan@users.noreply.github.com>
2022-02-22 19:44:47 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -4,10 +4,10 @@ async function handler(m) {
                     + 'VERSION:3.0\n' 
                     + 'N:;KingOfBear;;;'
                     + 'FN:KingOfBear\n' // full name
-                    + 'ORG:Owner YukiBot;\n' // the organization of the contact
-                    + 'TEL;type=CELL;type=VOICE;waid=6288279268363:+6288279268363\n' // WhatsApp ID + phone number
+                    + 'ORG:Owner FiraChann;\n' // the organization of the contact
+                    + 'TEL;type=CELL;type=VOICE;waid=6283830815715:+6283830815715\n' // WhatsApp ID + phone number
                     + 'END:VCARD'
-                conn.sendMessage(m.chat, { contacts: { displayName: 'King Of Bear', contacts: [{ vcard }] } }, { quoted: m })
+                conn.sendMessage(m.chat, { contacts: { displayName: 'F I R A', contacts: [{ vcard }] } }, { quoted: m })
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
